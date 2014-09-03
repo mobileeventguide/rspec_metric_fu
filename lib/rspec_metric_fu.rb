3d6cdffd0a2b8ec76ae41f3ac3dc20eda69638cb
Martin Schneider <info@outsmartin.de>
2014-09-03 09:42:04 +0200

    fixed metrics

--- lib/rspec_metric_fu.rb
+++ lib/rspec_metric_fu.rb
@@ -1,5 +1,6 @@
 require 'rspec_metric_fu/version'
 require 'simplecov'
+require 'metric_fu'
 require 'metric_fu/metrics/rcov/simplecov_formatter'
 
 module RspecMetricFu
@@ -7,7 +8,7 @@ module RspecMetricFu
     # run this method in your spec_helper
     def self.start
       SimpleCov.formatter = SimpleCov::Formatter::MetricFu
-      SimpleCov.start
+      SimpleCov.start 'rails'
     end
   end
 end
